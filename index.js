173863dc181102ca15279a2c6ba0309094d140e4
Michelle Weekes <michelleweekes@wired2grafix.com>
2019-06-01 11:56:28 -0400

    created tbl Classroom

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ const sqlite3 = require("sqlite3").verbose();
 
 const db = new sqlite3.Database(":memory:");
 
+db.serialize(function() {
+    db.run("CREATE TABLE Classroom (Building TEXT, Room_number NUMBER, Capacity NUMBER)");
+
+    db.run("INSERT INTO Classroom VALUES('Packard', 101, 500)");
+});
